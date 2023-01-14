73d8b8eae89ffb980c035e0fec5133b1ff2c6367
CaecEscobar <116753988+CaecEscobar@users.noreply.github.com>
2023-01-13 18:04:01 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,36 @@
+document.getElementById("gb").addEventListener("click", gb);
+document.getElementById("gbc").addEventListener("click", gbc);
+document.getElementById("gba").addEventListener("click", gba);
+
+function gb() {
+  document.getElementById("naroom").src = "public/naroom-gb.png";
+  document.getElementById("boxFamily").src = "public/boxFamilyGb.png";
+  document.getElementById("boxTech").src = "public/boxTechGb.png";
+  document.getElementById("boxWork").src = "public/boxWorkGb.png";
+  document.getElementById("boxHobbies").src = "public/boxHobbiesGb.png";
+  document.getElementById("boxAbout").src = "public/boxAboutGb.png";
+  document.getElementById("boxContact").src = "public/boxContactGb.png";
+  document.getElementById("css").href = "stylesGb.css";
+}
+
+function gbc() {
+  document.getElementById("naroom").src = "public/naroom-gbc.png";
+  document.getElementById("boxFamily").src = "public/boxFamilyGbc.png";
+  document.getElementById("boxTech").src = "public/boxTechGbc.png";
+  document.getElementById("boxWork").src = "public/boxWorkGbc.png";
+  document.getElementById("boxHobbies").src = "public/boxHobbiesGbc.png";
+  document.getElementById("boxAbout").src = "public/boxAboutGbc.png";
+  document.getElementById("boxContact").src = "public/boxContactGbc.png";
+  document.getElementById("css").href = "stylesGbc.css";
+}
+
+function gba() {
+  document.getElementById("naroom").src = "public/naroom-gba.png";
+  document.getElementById("boxFamily").src = "public/boxFamilyGba.png";
+  document.getElementById("boxTech").src = "public/boxTechGba.png";
+  document.getElementById("boxWork").src = "public/boxWorkGba.png";
+  document.getElementById("boxHobbies").src = "public/boxHobbiesGba.png";
+  document.getElementById("boxAbout").src = "public/boxAboutGba.png";
+  document.getElementById("boxContact").src = "public/boxContactGba.png";
+  document.getElementById("css").href = "stylesGba.css";
+}
