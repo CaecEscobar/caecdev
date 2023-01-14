b8556cbdca6eb6e8055be6663cbe9f070b6a8531
CaecEscobar <116753988+CaecEscobar@users.noreply.github.com>
2023-01-14 14:19:17 -0600

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -10,10 +10,21 @@ function gb() {
   document.getElementById("boxHobbies").src = "public/boxHobbiesGb.png";
   document.getElementById("boxAbout").src = "public/boxAboutGb.png";
   document.getElementById("boxContact").src = "public/boxContactGb.png";
+  document.getElementById("logohtml").src = "public/logo-html-gb.png";
+  document.getElementById("logocss").src = "public/logo-css-gb.png";
+  document.getElementById("logojs").src = "public/logo-javascript-gb.png";
+  document.getElementById("logobs").src = "public/logo-bootstrap-gb.png";
+  document.getElementById("logoreact").src = "public/logo-react-gb.png";
+  document.getElementById("familyimg0").src = "public/family-1-gb.png";
+  document.getElementById("workimg0").src = "public/work-cescobar-1-gb.png";
+  document.getElementById("workimg1").src = "public/work-cescobar-1-gb.png";
+  document.getElementById("workimg2").src = "public/work-cescobar-2-gb.png";
+  document.getElementById("workimg3").src = "public/work-cescobar-5-gb.png";
   document.getElementById("css").href = "stylesGb.css";
 }
 
 function gbc() {
+  document.getElementById("gbc").addEventListener("click", gbc);
   document.getElementById("naroom").src = "public/naroom-gbc.png";
   document.getElementById("boxFamily").src = "public/boxFamilyGbc.png";
   document.getElementById("boxTech").src = "public/boxTechGbc.png";
@@ -21,10 +32,21 @@ function gbc() {
   document.getElementById("boxHobbies").src = "public/boxHobbiesGbc.png";
   document.getElementById("boxAbout").src = "public/boxAboutGbc.png";
   document.getElementById("boxContact").src = "public/boxContactGbc.png";
+  document.getElementById("logohtml").src = "public/logo-html.png";
+  document.getElementById("logocss").src = "public/logo-css.png";
+  document.getElementById("logojs").src = "public/logo-javascript.png";
+  document.getElementById("logobs").src = "public/logo-bootstrap.png";
+  document.getElementById("logoreact").src = "public/logo-react.png";
+  document.getElementById("familyimg0").src = "public/family-1.jpg";
+  document.getElementById("workimg0").src = "public/work-cescobar-1.jpg";
+  document.getElementById("workimg1").src = "public/work-cescobar-1.jpg";
+  document.getElementById("workimg2").src = "public/work-cescobar-2.jpg";
+  document.getElementById("workimg3").src = "public/work-cescobar-5.jpg";
   document.getElementById("css").href = "stylesGbc.css";
 }
 
 function gba() {
+  document.getElementById("gba").addEventListener("click", gba);
   document.getElementById("naroom").src = "public/naroom-gba.png";
   document.getElementById("boxFamily").src = "public/boxFamilyGba.png";
   document.getElementById("boxTech").src = "public/boxTechGba.png";
@@ -32,5 +54,15 @@ function gba() {
   document.getElementById("boxHobbies").src = "public/boxHobbiesGba.png";
   document.getElementById("boxAbout").src = "public/boxAboutGba.png";
   document.getElementById("boxContact").src = "public/boxContactGba.png";
+  document.getElementById("logohtml").src = "public/logo-html.png";
+  document.getElementById("logocss").src = "public/logo-css.png";
+  document.getElementById("logojs").src = "public/logo-javascript.png";
+  document.getElementById("logobs").src = "public/logo-bootstrap.png";
+  document.getElementById("logoreact").src = "public/logo-react.png";
+  document.getElementById("familyimg0").src = "public/family-1.jpg";
+  document.getElementById("workimg0").src = "public/work-cescobar-1.jpg";
+  document.getElementById("workimg1").src = "public/work-cescobar-1.jpg";
+  document.getElementById("workimg2").src = "public/work-cescobar-2.jpg";
+  document.getElementById("workimg3").src = "public/work-cescobar-5.jpg";
   document.getElementById("css").href = "stylesGba.css";
 }
